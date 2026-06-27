d00c2a82804b630eb46b5acf2b6120db7a1b19ae
Ayza Aslam <itzaizaaslam@gmail.com>
2026-06-27 22:42:17 +0500

    Update page.js

--- client/src/app/signup/page.js
+++ client/src/app/signup/page.js
@@ -16,7 +16,7 @@ export default function Signup() {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:3000/signup", {
+      const response = await fetch("https://authkit-production-cd31.up.railway.app/signup", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
