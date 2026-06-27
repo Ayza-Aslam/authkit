ef0989775d1d21eb2c7e5061186c8d8dfc4ebb9e
Ayza Aslam <itzaizaaslam@gmail.com>
2026-06-28 00:44:57 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,7 +4,11 @@ const app = express();
 const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
 
-app.use(cors());
+app.use(cors({
+  origin: "https://authkit-tan.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
 app.use(express.json());
 
 app.use(authRoutes);
